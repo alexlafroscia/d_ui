fbfa99819fd5bd1b84698ca93d284827abe8ddf9
Alex LaFroscia <alex@lafroscia.com>
2021-08-04 23:06:28 -0400

    update List widget to write by row

--- lib/widgets/list.ts
+++ lib/widgets/list.ts
@@ -1,11 +1,9 @@
 import { DrawApi, Widget } from "./widget.ts";
 import { Colors } from "../color/mod.ts";
-import { Cell } from "../renderable/mod.ts";
+import { Row } from "../renderable/mod.ts";
 
-function makeCell(value: string, selected: boolean): Cell {
-  return selected
-    ? new Cell(value, Colors.Black, Colors.Blue)
-    : new Cell(value);
+function makeRow(value: string, selected: boolean): Row {
+  return selected ? new Row(value, Colors.Black, Colors.Blue) : new Row(value);
 }
 
 export class List implements Widget {
@@ -24,17 +22,13 @@ export class List implements Widget {
     this.selected = undefined;
   }
 
-  draw({ height, width, renderCell }: DrawApi) {
+  draw({ height, renderRow }: DrawApi) {
     const numberToDraw = Math.min(this.entries.length, height);
 
     for (let y = this.offset; y < numberToDraw; y++) {
       const entry = this.entries[y];
 
-      for (let x = 0; x < width; x++) {
-        const char = entry.charAt(x) || " ";
-
-        renderCell({ x, y }, makeCell(char, this.selected === y));
-      }
+      renderRow(y, makeRow(entry, this.selected === y));
     }
   }
 }
--- test/widgets/list.test.ts
+++ test/widgets/list.test.ts
@@ -1,41 +1,39 @@
-import { assertEquals, createView, stub } from "../helpers.ts";
+import { assertSpyCall, assertSpyCalls, createView, stub } from "../helpers.ts";
 import { List } from "../../lib/widgets/list.ts";
-import { Cell } from "../../lib/renderable/mod.ts";
+import { Row } from "../../lib/renderable/mod.ts";
 import { Colors } from "../../lib/color/mod.ts";
 
 Deno.test("writing entries", () => {
   const { view } = createView(3, 10);
   const list = new List(["first", "second"]);
-  const renderCell = stub(view, "renderCell");
+  const renderRow = stub(view, "renderRow");
 
   view.render(list);
 
-  assertEquals(renderCell.calls, [
-    ...[..."first".padEnd(10, " ")].map((char, index) => ({
-      args: [{ x: index, y: 0 }, new Cell(char)],
-      self: view,
-      returned: undefined,
-    })),
-    ...[..."second".padEnd(10, " ")].map((char, index) => ({
-      args: [{ x: index, y: 1 }, new Cell(char)],
-      self: view,
-      returned: undefined,
-    })),
-  ]);
+  assertSpyCall(renderRow, 0, {
+    args: [0, new Row("first")],
+    self: view,
+    returned: undefined,
+  });
+  assertSpyCall(renderRow, 1, {
+    args: [1, new Row("second")],
+    self: view,
+    returned: undefined,
+  });
+  assertSpyCalls(renderRow, 2);
 });
 
 Deno.test("rendering a selected entry", () => {
   const { view } = createView(3, 10);
   const list = new List(["first"], 0);
-  const renderCell = stub(view, "renderCell");
+  const renderRow = stub(view, "renderRow");
 
   view.render(list);
 
-  assertEquals(renderCell.calls, [
-    ...[..."first".padEnd(10, " ")].map((char, index) => ({
-      args: [{ x: index, y: 0 }, new Cell(char, Colors.Black, Colors.Blue)],
-      self: view,
-      returned: undefined,
-    })),
-  ]);
+  assertSpyCall(renderRow, 0, {
+    args: [0, new Row("first", Colors.Black, Colors.Blue)],
+    self: view,
+    returned: undefined,
+  });
+  assertSpyCalls(renderRow, 1);
 });
