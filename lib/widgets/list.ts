d499a2f25c65a0cf55a1ad0d04be4d63f87b2637
Alex LaFroscia <alex@lafroscia.com>
2021-08-10 19:28:37 -0400

    better control over List usage and rendering

--- lib/widgets/list.ts
+++ lib/widgets/list.ts
@@ -2,24 +2,52 @@ import { DrawApi, Widget } from "./widget.ts";
 import { Colors } from "../color/mod.ts";
 import { Row } from "../renderable/mod.ts";
 
-function makeRow(value: string, selected: boolean): Row {
-  return selected ? new Row(value, Colors.Black, Colors.Blue) : new Row(value);
-}
-
 export class List implements Widget {
-  private selected: number | undefined = undefined;
+  protected selectedIndex: number | undefined = undefined;
   private offset = 0;
 
   constructor(private entries: string[], selectedEntryIndex?: number) {
-    this.selected = selectedEntryIndex;
+    this.selectedIndex = selectedEntryIndex;
   }
 
-  selectEntries(index: number) {
-    this.selected = index;
+  get hasSelection(): boolean {
+    return typeof this.selectedIndex === "number";
+  }
+
+  get selected(): string | undefined {
+    return this.hasSelection ? this.entries[this.selectedIndex!] : undefined;
+  }
+
+  selectEntry(index: number) {
+    this.selectedIndex = index;
+  }
+
+  selectPrevious(increment = 1) {
+    if (this.hasSelection) {
+      this.selectedIndex = Math.max(
+        0, // Ftrst item
+        this.selectedIndex as number - increment, // Previous item
+      );
+    }
+  }
+
+  selectNext(increment = 1) {
+    if (this.hasSelection) {
+      this.selectedIndex = Math.min(
+        this.selectedIndex as number + increment, // Next item
+        this.entries.length - 1, // Last item
+      );
+    }
   }
 
   clearSelection() {
-    this.selected = undefined;
+    this.selectedIndex = undefined;
+  }
+
+  makeRow(entry: string, selected: boolean): Row {
+    return selected
+      ? new Row(entry, Colors.Black, Colors.Blue)
+      : new Row(entry);
   }
 
   draw({ height, renderRow }: DrawApi) {
@@ -28,7 +56,7 @@ export class List implements Widget {
     for (let y = this.offset; y < numberToDraw; y++) {
       const entry = this.entries[y];
 
-      renderRow(y, makeRow(entry, this.selected === y));
+      renderRow(y, this.makeRow(entry, this.selectedIndex === y));
     }
   }
 }
--- test/widgets/list.test.ts
+++ test/widgets/list.test.ts
@@ -1,4 +1,10 @@
-import { assertSpyCall, assertSpyCalls, createView, stub } from "../helpers.ts";
+import {
+  assertEquals,
+  assertSpyCall,
+  assertSpyCalls,
+  createView,
+  stub,
+} from "../helpers.ts";
 import { List } from "../../lib/widgets/list.ts";
 import { Row } from "../../lib/renderable/mod.ts";
 import { Colors } from "../../lib/color/mod.ts";
@@ -36,4 +42,96 @@ Deno.test("rendering a selected entry", () => {
     returned: undefined,
   });
   assertSpyCalls(renderRow, 1);
+});
+
+Deno.test("customizing the rendered Row", () => {
+  class RedList extends List {
+    makeRow(entry: string, selected: boolean): Row {
+      return selected
+        ? new Row(entry, Colors.White, Colors.Red)
+        : new Row(entry);
+    }
+  }
+  const { view } = createView(3, 10);
+  const list = new RedList(["first"], 0);
+  const renderRow = stub(view, "renderRow");
+
+  view.render(list);
+
+  assertSpyCall(renderRow, 0, {
+    args: [0, new Row("first", Colors.White, Colors.Red)],
+    self: view,
+    returned: undefined,
+  });
+  assertSpyCalls(renderRow, 1);
+});
+
+Deno.test("selecting an entry", () => {
+  const list = new List(["first", "second", "third"]);
+
+  assertEquals(list.selected, undefined);
+
+  list.selectEntry(0);
+
+  assertEquals(list.selected, "first");
+
+  list.clearSelection();
+
+  assertEquals(list.selected, undefined);
+});
+
+Deno.test("selecting the next entry when one is selected", () => {
+  const list = new List(["first", "second", "third"], 0);
+
+  assertEquals(list.selected, "first");
+
+  list.selectNext();
+
+  assertEquals(list.selected, "second");
+
+  list.selectNext();
+
+  assertEquals(list.selected, "third");
+
+  list.selectNext();
+
+  assertEquals(list.selected, "third");
+});
+
+Deno.test("selecting the next entry when one is not selected", () => {
+  const list = new List(["first", "second", "third"]);
+
+  assertEquals(list.selected, undefined);
+
+  list.selectNext();
+
+  assertEquals(list.selected, undefined);
+});
+
+Deno.test("selecting the previous entry when one is selected", () => {
+  const list = new List(["first", "second", "third"], 2);
+
+  assertEquals(list.selected, "third");
+
+  list.selectPrevious();
+
+  assertEquals(list.selected, "second");
+
+  list.selectPrevious();
+
+  assertEquals(list.selected, "first");
+
+  list.selectPrevious();
+
+  assertEquals(list.selected, "first");
+});
+
+Deno.test("selecting the previous entry when one is not selected", () => {
+  const list = new List(["first", "second", "third"]);
+
+  assertEquals(list.selected, undefined);
+
+  list.selectPrevious();
+
+  assertEquals(list.selected, undefined);
 });
