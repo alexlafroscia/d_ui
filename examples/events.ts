f7cbdc55ee06714037c5839d2ae796ac5dd66139
Alex LaFroscia <alex@lafroscia.com>
2022-04-20 12:08:08 -0400

    remove unnecessary event example

--- examples/events.ts
+++
@@ -1,8 +0,0 @@
-import * as log from "https://deno.land/std@0.102.0/log/mod.ts";
-import { eventStream } from "../lib/mod.ts";
-
-import "./setup-log.ts";
-
-for await (const event of eventStream(Deno.stdin)) {
-  log.info(event);
-}
