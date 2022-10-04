5bf8a0fe1fb0aaf1ed135aecc58fbb570c6721b2
Alex LaFroscia <alex@lafroscia.com>
2022-10-04 10:38:47 -0400

    expose `Point` type

--- lib/mod.ts
+++ lib/mod.ts
@@ -5,5 +5,6 @@ export * from "./color/mod.ts";
 export * from "./drawable/mod.ts";
 export * from "./events/mod.ts";
 export * from "./jsx.ts";
+export { type Point } from "./matrix/mod.ts";
 export * from "./renderable/mod.ts";
 export * from "./view/mod.ts";
